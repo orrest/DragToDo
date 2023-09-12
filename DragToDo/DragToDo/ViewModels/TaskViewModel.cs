61683303e11a134ab64056038f2df805fab511f6
zzhxufeng <zzhxufeng@163.com>
2023-09-12 16:02:29 +0800

    TaskView: task tabs

--- DragToDo/DragToDo/ViewModels/TaskViewModel.cs
+++ DragToDo/DragToDo/ViewModels/TaskViewModel.cs
@@ -1,9 +1,10 @@
-﻿using ReactiveUI;
+﻿using DragToDo.Navigation;
+using ReactiveUI;
 using System;
 
 namespace DragToDo.ViewModels;
 
-public class TaskViewModel : ViewModelBase, IRoutableViewModel
+public class TaskViewModel : ViewModelBase, IRoutableViewModel, IScreen
 {
     // Unique identifier for the routable view model.
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
@@ -11,8 +12,21 @@ public class TaskViewModel : ViewModelBase, IRoutableViewModel
     // Reference to IScreen that owns the routable view model.
     public IScreen HostScreen { get; }
 
-    public TaskViewModel(IScreen screen)
+    public RoutingState Router { get; } = new RoutingState();
+
+    public TaskViewModel(IScreen hostScreen)
     {
-        HostScreen = screen;
+        HostScreen = hostScreen;
+    }
+}
+
+public class TaskMenuItem : MenuItem
+{
+    public TaskMenuItem(
+        string name,
+        RoutingState router,
+        Type type,
+        IScreen screen) : base(name, router, type, screen)
+    {
     }
 }
